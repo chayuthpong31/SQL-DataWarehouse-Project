0b2f0be92a830a96ac64e756a886e5d35d4bfd7b
chayuthpong31 <chayuthpong31@gmail.com>
2025-12-16 16:12:30 +0700

    transfrom crm silver

---
+++ scripts/silver/validation_silver.sql
@@ -0,0 +1,135 @@
+/*
+=============================================
+Validation data correctness 
+=============================================
+This scripts checking quality of data such as duplicate, space in fistname ,lastname, key and gender, consistency data.
+*/
+
+-- =====================================================
+-- Check For Nulls or Duplicates in Primary Key
+-- Expectation: No Result
+SELECT 
+cst_id,
+COUNT(*)
+FROM silver.crm_cust_info
+GROUP BY cst_id
+HAVING COUNT(*) > 1 OR cst_id IS NULL
+
+-- Check for unwanted Spaces
+-- Expectation: No Result
+SELECT cst_firstname
+FROM silver.crm_cust_info
+WHERE cst_firstname != TRIM(cst_firstname)
+
+-- Check for unwanted Spaces
+-- Expectation: No Result
+SELECT cst_lastname
+FROM silver.crm_cust_info
+WHERE cst_lastname != TRIM(cst_lastname)
+
+-- Check for unwanted Spaces
+-- Expectation: No Result
+SELECT cst_gender
+FROM silver.crm_cust_info
+WHERE cst_gender != TRIM(cst_gender)
+
+-- Check for unwanted Spaces
+-- Expectation: No Result
+SELECT cst_key
+FROM silver.crm_cust_info
+WHERE cst_key != TRIM(cst_key)
+
+-- Data Standardization & Consistency
+SELECT DISTINCT cst_gender
+FROM silver.crm_cust_info
+
+-- Data Standardization & Consistency
+SELECT DISTINCT cst_marital_status
+FROM silver.crm_cust_info
+
+SELECT * FROM silver.crm_cust_info
+-- =====================================================
+
+-- =====================================================
+-- crm_prd_info table
+
+-- Check For Nulls or Duplicates in Primary Key
+-- Expectation: No Result
+SELECT 
+prd_id,
+COUNT(*)
+FROM silver.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL
+
+-- Check for unwanted Spaces
+-- Expectation: No Result
+SELECT prd_nm
+FROM silver.crm_prd_info
+WHERE  prd_nm != TRIM(prd_nm)
+
+-- Check for NULLs or Negative Numbers
+-- Expectation: No Result
+SELECT prd_cost
+FROM silver.crm_prd_info
+WHERE  prd_cost < 0 OR prd_cost IS NULL
+
+-- Data Standardization & Consistency
+SELECT DISTINCT prd_line
+FROM silver.crm_prd_info
+
+-- Check for Invalid Date Orders
+-- Expectation: No Result
+SELECT *
+FROM silver.crm_prd_info
+WHERE prd_end_dt < prd_start_dt
+-- =====================================================
+
+-- =====================================================
+-- crm_sales_details
+
+-- Check for Invalid Order Dates
+SELECT
+NULLIF(sls_order_dt,0) sls_order_dt
+FROM bronze.crm_sales_details
+WHERE sls_order_dt <= 0 
+OR LEN(sls_order_dt) != 8 
+OR sls_order_dt > 20500101 
+OR sls_order_dt < 19000101
+
+-- Check for Invalid Ship Dates
+SELECT
+NULLIF(sls_ship_dt,0) sls_ship_dt
+FROM bronze.crm_sales_details
+WHERE sls_ship_dt <= 0 
+OR LEN(sls_ship_dt) != 8 
+OR sls_ship_dt > 20500101 
+OR sls_ship_dt < 19000101
+
+-- Check for Invalid Ship Dates
+SELECT
+NULLIF(sls_due_dt,0) sls_due_dt
+FROM bronze.crm_sales_details
+WHERE sls_due_dt <= 0 
+OR LEN(sls_due_dt) != 8 
+OR sls_due_dt > 20500101 
+OR sls_due_dt < 19000101
+
+-- Check for Invalid Date Orders
+SELECT
+*
+FROM silver.crm_sales_details
+WHERE sls_order_dt > sls_ship_dt OR sls_ship_dt > sls_due_dt
+
+-- Check Data Consistency: Between Sales, Quantity, and Price
+-- >> Sales = Quantity * Price
+-- >> Values must not be NULL, zero or negative.
+SELECT
+sls_sales,
+sls_quantity,
+sls_price
+FROM silver.crm_sales_details
+WHERE sls_sales != sls_quantity * sls_price
+OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
+OR sls_sales <= 0 OR sls_quantity <= 0 OR sls_price <= 0
+-- =====================================================
